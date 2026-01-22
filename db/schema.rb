4162bd65ae3042fa8ded4331cae64b1f871b2b0e
Kaisser Sereno <serenokaisser.dev@gmail.com>
2026-01-22 14:47:06 +0800

    Add reference for post and user done

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_01_22_061215) do
+ActiveRecord::Schema[8.1].define(version: 2026_01_22_062632) do
   create_table "posts", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.text "description"
     t.string "title"
     t.datetime "updated_at", null: false
+    t.integer "user_id", null: false
+    t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -23,4 +25,6 @@ ActiveRecord::Schema[8.1].define(version: 2026_01_22_061215) do
     t.datetime "updated_at", null: false
     t.string "username"
   end
+
+  add_foreign_key "posts", "users"
 end
