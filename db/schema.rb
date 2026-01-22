2eb9869ea1a83c7c1c58f9d989db24e048f1b3cc
Kaisser Sereno <serenokaisser.dev@gmail.com>
2026-01-22 14:26:06 +0800

    Added Post model and its initial migration file

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_01_22_055106) do
+ActiveRecord::Schema[8.1].define(version: 2026_01_22_061215) do
+  create_table "posts", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.text "description"
+    t.string "title"
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
